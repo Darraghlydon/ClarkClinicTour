747967f1d132c773dd46b7159e10ff2ac354f7e5
ataskerlynch <146075853+ataskerlynch@users.noreply.github.com>
2025-11-18 13:14:08 +0000

    FPS Counter Update

---
+++ Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI _fpsGUIText;
+    private int _frameCount;
+    private float _elapsedTime;
+    void Update()
+    {
+        _frameCount++;
+        _elapsedTime += Time.deltaTime;
+        if (_elapsedTime >= 1f)
+        {
+            _fpsGUIText.text = "FPS: " + _frameCount.ToString();
+            _frameCount = 0;
+            _elapsedTime = 0f;
+        }
+    }
+
+    // Optional: Provide a way to access the frame count
+    public int GetFrameCount()
+    {
+        return _frameCount;
+    }
+}
